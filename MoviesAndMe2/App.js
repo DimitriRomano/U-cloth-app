08af7ad055f3c143b0efb314f5fb833d98b40886
DimitriRomano <dimitri.romano09@gmail.com>
2021-12-15 15:33:40 +0100

    expo

--- MoviesAndMe2/App.js
+++ MoviesAndMe2/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import Navigation from './Navigation/Navigation';
 import { Provider } from 'react-redux';
 import Store from './Store/configureStore'
+import LoginScreen from './Components/LoginScreen';
 
 export default function App() {
   return (
     <Provider store={Store}>
       <Navigation/>
     </Provider>
-    // <Test/>
+    // <LoginScreen/>
   );
 }
